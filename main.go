e4cd18ffcfdce632a979943bd032044878d40f24
EnricoPDG <ricopapsch@hotmail.com>
2022-05-18 13:43:20 -0300

    Modularizando o código

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"net/http"
+	"text/template"
+	"github.com/EnricoPDG/GoWebFund/models"
+	_ "github.com/lib/pq"
+)
+
+var temp = template.Must(template.ParseGlob("templates/*.html"))
+
+func main() {
+	http.HandleFunc("/", index)
+	http.ListenAndServe(":8000", nil)
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	todosOsProdutos := models.BuscaTodosOsProdutos()
+	temp.ExecuteTemplate(w, "Index", todosOsProdutos)
+}
